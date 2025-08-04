cd159ec408c02d3bd5505a3b59f8ac0e1b2a7a81
Horn KimEang <eangdev.gamedev@gmail.com>
2025-08-04 20:29:29 +0700

    addButton

--- Cafe-PoS/Cafe-PoS/Form1.Designer.cs
+++ Cafe-PoS/Cafe-PoS/Form1.Designer.cs
@@ -28,23 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.BG = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.MainPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.BG)).BeginInit();
             this.SuspendLayout();
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.BackColor = System.Drawing.Color.White;
+            this.MainPanel.Controls.Add(this.label1);
+            this.MainPanel.Controls.Add(this.BG);
+            this.MainPanel.Location = new System.Drawing.Point(0, 0);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(800, 550);
+            this.MainPanel.TabIndex = 0;
+            // 
+            // BG
+            // 
+            this.BG.Image = global::Cafe_PoS.Properties.Resources.Cafe_LoginBG1;
+            this.BG.Location = new System.Drawing.Point(0, 0);
+            this.BG.Name = "BG";
+            this.BG.Size = new System.Drawing.Size(800, 550);
+            this.BG.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BG.TabIndex = 0;
+            this.BG.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(48)))), ((int)(((byte)(71)))));
+            this.label1.Font = new System.Drawing.Font("Noto Sans SC", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(89, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(284, 79);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Cafe-POS";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(64)))), ((int)(((byte)(164)))));
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 550);
+            this.Controls.Add(this.MainPanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
+            this.MainPanel.ResumeLayout(false);
+            this.MainPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.BG)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.PictureBox BG;
+        private System.Windows.Forms.Label label1;
     }
 }
 
