c3a3aabd75f8c03787f832f659cd596705da6cf5
Horn KimEang <eangdev.gamedev@gmail.com>
2025-08-04 20:08:52 +0700

    addBg

--- Cafe-PoS/Cafe-PoS/Form1.Designer.cs
+++ Cafe-PoS/Cafe-PoS/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Cafe_PoS
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(64)))), ((int)(((byte)(164)))));
+            this.ClientSize = new System.Drawing.Size(800, 550);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
